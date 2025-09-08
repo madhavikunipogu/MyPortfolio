21a74f9afa11c997587819ff3f14eb9b6c607fe0
Madhaviproject <madhavikunipogu123@gmail.com>
2025-09-08 20:23:20 +0530

    last updated on 9/8/25 8pm

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// reference
+export const environment = {
+  production: false,
+  emailServiceID: 'yours serviceid',
+  emailTemplateID: 'yours templateid',
+  emailPublicKey: 'yours publickey'
+};
+// use environment.prod.ts in your project
